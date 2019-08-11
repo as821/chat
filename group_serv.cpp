dbb58c9182c23e18855e544fbe9e7a0a734ad828
as821 <49156741+as821@users.noreply.github.com>
2019-08-11 17:10:22 -0400

    Fully support group file sending

--- group_serv.cpp
+++ group_serv.cpp
@@ -1,76 +1,87 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+
+// include statements
+#include <sys/types.h>          // socket struct headers
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <arpa/inet.h>          // internet headers
+#include <netinet/in.h>
+#include <unistd.h>
+
+#include <stdexcept>            // additional headers
+#include <iostream>
+#include <stdio.h>
+#include <stdint.h>
+#include <string>
+#include <sys/time.h>
+#include <fstream>
+#include <fcntl.h>
+#include <pthread.h>
+#include <vector>
+#include <errno.h>
 
 
-// server_chat()        More advanced client message handling
-void server_chat(int sockfd) {
-    // general set up
-    char sendline[MAXLINE], recvline[MAXLINE], overread[BYTES_IN_LONG];
-    uint32_t num_bytes = 0;
-    bool get_bits = true;
-    bool expect_file = false;
+// constants
+#define MAXLINE             4096            // max buffer length
+#define BYTES_IN_LONG       4               // num bytes in a uint32_t
+#define LISTENQ             1024            // size of listen queue
+#define MAX_CONNECTION      100             // max number of connections
+#define STDIN               0               // std input fd is 0
+#define PROTOCOL_MSG_LEN    12              // protocol message length (arbitrary)
+#define DEST_FILENAME   ""                  // default destination file
+#define SRC_FILENAME    ""                  // default source file
 
-    // file descriptor/select() prep
-    struct timeval tv;
-    fd_set readfd, master;
-    int maxfd;                                                  // record max fd
-    FD_ZERO(&master);                                           // zero set structures
-    FD_ZERO(&readfd);
-    FD_SET(sockfd, &master);                                    // add sockfd to master set
-    FD_SET(STDIN, &master);                                     // add standard input to master set
-    maxfd = sockfd;                                             // sockfd > 0 by definition
 
-    std::cout << "input text...\n";                             // prompt user for input
-    while (true) {                                              // infinite loop to wait for socket input or I/O
-        readfd = master;                                        // copy master to readfd since select updates contents
-        Select(maxfd+1, &readfd, NULL, NULL, NULL);             // select() call to update fd_set struct
-
-        if(FD_ISSET(sockfd, &readfd)) {                         // sockfd is set...
-            int n = 0;
-            if(!expect_file) {                                  // not expecting a file
-               if ((n = read(sockfd, recvline, MAXLINE)) == 0) // read from socket
-                    throw std::invalid_argument("client disconnect error...");
-                recvline[n] = '\0';                             // ensure null-termination
-                input_filtering(recvline, overread, &num_bytes, &expect_file, &get_bits, n);    // check for keywords
-            }
-            if(expect_file) {                                   // file expected from client
-                std::cout << "file expected...\n";
-                file_recv_handling(sockfd, recvline, DEST_FILENAME, num_bytes, get_bits);   // handle file reception
-                expect_file = false;                            // reset file expected flag
-            }
-        }
-        if(FD_ISSET(STDIN, &readfd)) {                          // standard IO fd set...
-            fgets(sendline, MAXLINE-1, stdin);                  // stop input if \n or EOF. \0 terminate + check buff sz
-            Write(sockfd, sendline, strlen(sendline));          // write input to socket
-        }
-    }
-}   // END server_chat()
+extern int message_id_counter;              // must be global so Group_Message constructor has access
 
 
 
-// server()     Deals with one client only. 1:1 connections
-void server(int port_input) {
-    // declare variables
-    int listenfd, connfd;
-    socklen_t clilen;
-    struct sockaddr_in servaddr, clientaddr;
+// Group_Message struct definition
+struct Group_Message {
+    char* msg;                              // message text
+    size_t msg_len;
+    pthread_t have_sent [MAX_CONNECTION];   // store thread ids of all threads that have sent message
+    int num_sent;                           // have_sent size
+    int message_id;                         // ID number
+    bool msg_dynamic_alloc;                 // true if msg has been dynamically allocated (if needs to be deleted)
 
-    // fill struct
-    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port_input);
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    Group_Message();                        // constructor (assign message_id and allocate for msg)
+    ~Group_Message();                       // destructor
+};
 
-    // bind and listen to socket
-    Bind(listenfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
-    Listen(listenfd, LISTENQ);
-    std::cout << "waiting for connection...\n";
 
-    // accept a single client connection
-    clilen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (struct sockaddr*) &clientaddr, &clilen);
 
-    // interact with client
-    std::cout << "connected...\n";
-    server_chat(connfd);
-}   // END server()
+//  top-level functions
+void server(int port_input);
+void client(int port_input, std::string ip_input);
+void group_serv(int port_input);
+
+// file sending/reception
+void send_file(std::string file_path, int sockfd);
+void file_recv_handling(int sockfd, char* recvline, std::string filename, uint32_t num_bytes, bool get_bits);
+void input_filtering(char* recvline, char* overread, uint32_t* num_bytes, bool* expect_file, bool* get_bits, int n);
+
+// group messaging functions
+void* group_handler(void* sockfd);
+bool find_pthread(pthread_t* src, int size, pthread_t search_val );
+Group_Message* message_id_find(int message_id, std::vector<Group_Message*>* message_stack_ptr, int* offset );
+void input_filtering_group(char* recvline, char* overread, uint32_t* num_bytes, bool* expect_file, bool* get_bits, int n);
+void file_recv_handling_group(int sockfd, char* recvline, std::string filename, uint32_t num_bytes, bool get_bits);
+
+// system call wrappers (error checking)
+int Socket (int family, int type, int protocol);
+void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+void Listen(int listenfd, int backlog);
+int Accept (int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+int Write (int fd, const void *buf, size_t count);
+void Connect(int sockfd, struct sockaddr* address, int addr_len);
+void Inet_pton(int family, const char* strptr, void* addrptr );
+void Select(int numfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfds, struct timeval* timeout);
+
+// other
+void print_machine_ip();
+
+
+#endif // MAIN_H
